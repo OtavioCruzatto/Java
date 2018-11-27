c320857d4b601fb6eb517521e06ee6ae075a0a03
Otavio Cruzatto <30843450+OtavioCruzatto@users.noreply.github.com>
2018-11-27 16:59:26 -0200

    Add files via upload

--- Generico/src/starter/Starter.java
+++ Generico/src/starter/Starter.java
@@ -1,44 +1,14 @@
 package starter;
 
-import portaSerial.Baudrate;
-import portaSerial.ControleDeFluxo;
-import portaSerial.DataBits;
-import portaSerial.Paridade;
-import portaSerial.PortaSerial;
-import portaSerial.StopBits;
+import crc16.CRC16;
 
 public class Starter {
 
 	public static void main(String[] args) {
 
-		PortaSerial com = new PortaSerial();
-		com.setPortaComSelecionada(com.getListaDePortasComDisponiveis()[0]);
-		com.setBaudrate(Baudrate.Baud_115200.getBaudrate());
-		com.setParidade(Paridade.None.getParidade());
-		com.setDataBits(DataBits.DataBits_8_bits.getDataBits());
-		com.setControleDeFluxo(ControleDeFluxo.None.getControleDeFluxo());
-		com.setStopBits(StopBits.StopBit_1.getStopBits());
-		com.conectarNaPorta();	
-		com.configurarPortaSerial();
-			
+		System.out.println(Integer.toHexString(CRC16.calcularCrc16ASCII("123456789")));
+		System.out.println(Integer.toHexString(CRC16.calcularCrc16HEX("AA 55 D0 25 9E 9D 9C")));
 		
-		while(true) {
-			
-			try {
-				Thread.sleep(750);
-				for(int aux = 0; aux < com.getDadosRecebidosByte().size(); aux++) {
-					System.out.print(Integer.toHexString(com.getDadosRecebidosByte().get(aux)) + " ");
-				}
-				System.out.println();
-				
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			
-		}
-
 	}
 
 }
